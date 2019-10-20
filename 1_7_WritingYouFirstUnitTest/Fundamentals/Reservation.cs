ceddb56abe7c6e361cebacee03a1d55832635b53
thepnarin <43739993+thepnarin@users.noreply.github.com>
2019-10-20 13:22:56 +0700

    WritingYouFirstUnitTest

---
+++ 1_7_WritingYouFirstUnitTest/Fundamentals/Reservation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_7_WritingYouFirstUnitTest
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            if (user.IsAdmin)
+                return true;
+
+            if (MadeBy == user)
+                return false;
+
+            return false;
+        }
+
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
